d4e4ce25562433c9ef1035346a069bcdb774c1db
longsong392 <2790982254@qq.com>
2026-05-05 11:43:34 +0800

    fix: 配置文件

---
+++ config/config_example.h
@@ -0,0 +1,13 @@
+#ifndef CONFIG_EXAMPLE_H
+#define CONFIG_EXAMPLE_H
+
+/* WIFI */
+#define WIFI_SSID           "wifi_ssid"
+#define WIFI_PASSWORD       "password"
+
+/* OneNET */
+#define ONENET_CLIENT_ID    "test"                                          //此处填设备名称
+#define ONENET_USERNAME     "123"                                    //此处填设备ID
+#define ONENET_PASSWORD     "123456" //此处填设备Token
+
+#endif
